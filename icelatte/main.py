d5a8e2fee3ef75ef8c37b77fc0a92106f6749782
Flaouve <flaouve@gmail.com>
2026-05-23 00:24:36 +0300

    feat: add say_embed and testembed commands

--- icelatte/main.py
+++ icelatte/main.py
@@ -16,7 +16,12 @@ TOKEN = os.getenv("TOKEN")
 bot = discord.Bot()
 
 @bot.slash_command()
-async def hello(ctx, name: str = None, nachricht: str = None):
+async def hello(
+    ctx, 
+    name: str = None, 
+    nachricht: str = None
+    ):
+
     name = name or ctx.author.name
     if nachricht == None:
         await ctx.respond(f"Hello {name}")
@@ -24,7 +29,12 @@ async def hello(ctx, name: str = None, nachricht: str = None):
         await ctx.respond(f"Hello {name}, {nachricht}")
 
 @bot.slash_command()
-async def say(ctx, nachricht: str = None, mention_author: bool = True):
+async def say(
+    ctx, 
+    nachricht: str = None, 
+    mention_author: bool = True
+    ):
+
     nachricht = nachricht or ""
     if mention_author:
         await ctx.respond(f"{ctx.author.mention} says: {nachricht}")
@@ -32,12 +42,45 @@ async def say(ctx, nachricht: str = None, mention_author: bool = True):
         await ctx.respond(f"{ctx.author.name} says: {nachricht}")
 
 @bot.slash_command()
-async def invate(ctx):
+async def say_embed(
+    ctx, 
+    nachricht: str = None
+    ):
+
+    nachricht = nachricht or ""
+    await ctx.respond(embed = discord.Embed(
+        title=ctx.author.name+": Says",
+        description=nachricht,
+        color=0x74456e
+    ))
+
+@bot.slash_command()
+async def invate(
+    ctx
+    ):
+
     await ctx.respond("Invate Mee yeyy:\nhttps://discord.com/oauth2/authorize?client_id=1505951400281247825&permissions=4503599627373568&integration_type=0&scope=bot")
         
-@bot.user_command(name="Say Hello")
+@bot.slash_command(
+    name="testembed", 
+    description="test embed."
+    )
+async def testembed(
+    ctx
+    ):
+
+    embed = discord.Embed(
+        title="Test Embed",
+        description="Das ist ein Test Embed.",
+        color=0x74456e
+    )
+    await ctx.respond(embed=embed)
+
+@bot.user_command(
+    name="Say Hello"
+    )
+
 async def hi(ctx, user):
     await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
-
-
+    
 bot.run(TOKEN)
